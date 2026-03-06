c963c34870f9c1fb527cf09d23e24f365f641572
stidalexis <stidalexis@gmail.com>
2026-03-06 13:32:05 -0500

    programa_nuve.py

--- programa_nuve.py
+++ programa_nuve.py
@@ -317,8 +317,9 @@ elif menu == "📅 Planificación":
                 pres = g4.selectbox("Encolada o Grapada", PRESENTACIONES2)
                 
                 p1, p2 = st.columns(2)
-                num_i = p1.number_input("Numeracion Desde", 0)
-                num_f = p2.number_input("Numeracion Hasta", 0)
+                 t_num = p1.selectbox("¿Tiene Numeracion?", ["NO", "SI"])
+                num_id = p1.text_area("Numeracion Desde") if t_perf == "SI" else "NO"
+                num_fd = p2.text_area("Numeracion Hasta") if t_barr == "SI" else "NO"
                 t_perf = p1.selectbox("¿Tiene Perforaciones?", ["NO", "SI"])
                 perf_d = p1.text_area("Detalle Perforación") if t_perf == "SI" else "NO"
                 t_barr = p2.selectbox("¿Tiene Código de Barras?", ["NO", "SI"])
@@ -504,6 +505,7 @@ elif menu in ["🖨️ Impresión", "✂️ Corte", "📥 Colectoras", "📕 Enc
                     st.session_state.rep = None
                     st.success(f"Trabajo Finalizado. OP movida a: {n_area}")
                     time.sleep(1.5); st.rerun()
+
 
 
 
