8541882c682472f43066db7746b356f9141701b6
stidalexis <stidalexis@gmail.com>
2026-03-13 16:21:35 -0500

    programa_nuve.py

--- programa_nuve.py
+++ programa_nuve.py
@@ -91,14 +91,15 @@ def to_excel_limpio(df_input, tipo=None):
 def generar_pdf_op(row):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_text_color(255,255,255)
-    pdf.set_font("Arial","B",20)
-    pdf.set_xy(0,8)
-    pdf.cell(0,20,"CERTIFICADO",0,1,"C")
+   pdf.set_text_color(255,255,255)
 
-    pdf.set_font("Arial","",14)
-    pdf.set_xy(0,18)
-    pdf.cell(0,20,"DE PRODUCCION",0,1,"C")
+    pdf.set_font("Arial","B",22)
+    pdf.set_xy(0,8)
+    pdf.cell(210,10,"CERTIFICADO",0,1,"C")
+
+    pdf.set_font("Arial","",15)
+    pdf.set_xy(0,20)
+    pdf.cell(210,10,"DE PRODUCCION",0,1,"C")
     # ===== ENCABEZADO 40mm =====
 
     # Azul principal
