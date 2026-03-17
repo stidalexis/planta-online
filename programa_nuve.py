bca99d22a7354dcc9cf6aa88093ff7c41b483025
stidalexis <stidalexis@gmail.com>
2026-03-17 13:28:32 -0500

    programa_nuve.py

--- programa_nuve.py
+++ programa_nuve.py
@@ -947,8 +947,8 @@ elif menu == "📅 Planificación":
                         "unidades_caja": int(uc),
                         "observaciones_rollos": obs,
                         "ref_comercial": ref_c,
-                        "transportadora_rollos": tra,
-                        "destino_rollos": c_tra
+                        "transportadora_rollos": t_trans_f,
+                        "destino_rollos": dest_f
                     })
 
                 supabase.table("ordenes_planeadas").insert(payload).execute()
